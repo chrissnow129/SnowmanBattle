ed70c349a2c411a849becba35819d665b9c023c9
chris <chris@chriss-iMac.local>
2020-12-07 23:34:43 -0800

    added pics & started enemy class

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 $(() => {
-  const introMod = $("<div>")
-    .addClass("intromod")
-    .appendTo("body");
-    
-    const h2 = $("<h2>")
-      .text('Welcome Traveler!')
-      .css('font-family', 'Nunito')
-      .appendTo(introMod);
+  const introMod = $("<div>").addClass("intromod").appendTo("body");
 
-    const p = $("<p>")
-      .text('As you trek through the Alaskan wilderness')
-      .appendTo(introMod);
+  const h2 = $("<h2>")
+    .text("Welcome Traveler!")
+    .css("font-family", "Nunito")
+    .appendTo(introMod);
 
-    const startbut = $("<button>")
-      .attr('id','starbut')
-      .text('Start Game')
-      .appendTo(introMod);
+  const p = $("<p>")
+    .text("As you trek through the Alaskan wilderness")
+    .appendTo(introMod);
 
+  const startbut = $("<button>")
+    .attr("id", "starbut")
+    .text("Start Game")
+    .appendTo(introMod);
 
-const startgame = () => {
-    introMod.hide()
-}
+  const snowhench = $("#snow-hench");
 
-startbut.on('click', startgame);
+  const startgame = () => {
+    introMod.hide();
+    snowhench.css("display", "block");
+  };
+
+  class Snowman{
+      constructor(health, attack) {
+          this.health = 10
+          this.attack = attack
+      }
+  }
+
+  startbut.on("click", startgame);
 });
