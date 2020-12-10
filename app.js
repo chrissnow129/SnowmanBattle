c641cb1e7f95223a4e8ec10c58bea765dff8bd91
ChrisSnowman <csnowman129@gmail.com>
2020-12-09 20:24:41 -0800

    added SnowBoss class & instance. alsoalso made progress on attacks

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ $(() => {
     introMod.hide();
     snowhench.css("display", "block");
     brody.css("display", "block");
-    lantBut.css("display", "block")
+    lantBut.css("display", "block");
   };
 
   const lantBut = $("<button>")
-      .attr("id", "lantbut")
-      .text("Use a Warm Lantern")
-      .appendTo("body");
+    .attr("id", "lantbut")
+    .text("Use a Warm Lantern")
+    .appendTo("body");
 
   class Snowman {
     constructor(name, health, attacks) {
@@ -36,29 +36,34 @@ $(() => {
       this.health = 10;
       this.attacks = {
         freezeBlast: 2,
-        icicleGun: 4
+        icicleGun: 4,
       };
     }
     freezeBlast() {
-      hero.health -= this.attacks.freezeBlast
-      console.log(`The Frostbite hit you with an icy shockwave and now you have ${hero.health} HP`)
+      hero.health -= this.attacks.freezeBlast;
+      console.log(
+        `The Frostbite hit you with an icy shockwave and now you have ${hero.health} HP`
+      );
     }
 
     icicleGun() {
-      hero.health -= this.attacks.icicleGun
-      console.log(`You got hit with an icicle and now have ${hero.health} HP`)
+      hero.health -= this.attacks.icicleGun;
+      console.log(`You got hit with an icicle and now have ${hero.health} HP`);
     }
   }
 
-  const Frostbite = new Snowman("Frostbite")
+  const Frostbite = new Snowman("Frostbite");
 
   class SnowBoss extends Snowman {
-    constructor(regen){
-    super(name, health, attacks);
-    this.name = ''
+    constructor(regen, name, health, attacks) {
+      super(name, health, attacks);
+      this.name = name;
+      this.health = 15;
+      this.regen = 2;
     }
-
   }
+
+  const CaptainIce = new SnowBoss("Captain Shivers");
 
   class Traveler {
     constructor(name, health) {
@@ -75,11 +80,27 @@ $(() => {
         `You have melted a little of the Frostbite leaving him at ${enemy.health}`
       );
     }
-  }
+  };
+
+  const loseMod = () => {
+    $("<div>").addClass("youLost").appendTo("body");
+  };
 
   const hero = new Traveler("Brody");
   console.log(hero);
 
+  if (hero.health <= 0) {
+    loseMod();
+  }
+
+  let gameLevel = 1;
+
   startbut.on("click", startgame);
-  lantbut.on("click", hero.lanternMelt());
+  lantBut.on("click", () => {
+    if (gameLevel = 1) {
+      hero.lanternMelt(Frostbite);
+    } else {
+      hero.lanternMelt(CaptainIce);
+    }
+  });
 });
